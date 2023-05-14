7c075cfc5f53b74112840cf48c72e9718267c989
anafri96 <chana-aviva.friberg@hotmail.com>
2023-05-14 16:04:48 +0300

    photos

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,8 +1,8 @@
-import styles from "./post.module.css"
+import styles from "./post.module.css";
 
 export async function getPostById(postId: string) {
   const res = await fetch(
-    `${process.env.REACT_APP_API_URL}/${postId}`
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
   );
 
   if (!res.ok) {
@@ -18,16 +18,16 @@ export default async function PostPage({
   params: { id: string };
 }) {
   const post = await getPostById(id);
-//   console.log(post, "anan");
+  //   console.log(post, "anan");
   return (
     <>
-       <div>
-      <div className="bg-white shadow-md rounded-lg p-4 mb-4">
-        <p className={styles.red}>ANA</p>
-        <h3 className="text-xl font-bold mb-2">{post.title}</h3>
-        <p className="text-gray-700">ID: {post.id}</p>
+      <div>
+        <div className="bg-white shadow-md rounded-lg p-4 mb-4">
+          <p className={styles.red}>ANA</p>
+          <h3 className="text-xl font-bold mb-2">{post.title}</h3>
+          <p className="text-gray-700">ID: {post.id}</p>
+        </div>
       </div>
-    </div>
     </>
   );
 }
