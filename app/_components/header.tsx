57e80b4fe93e5353b2a12c5d48c25829bb1f8f12
anafri96 <chana-aviva.friberg@hotmail.com>
2023-05-14 16:17:23 +0300

    no photos

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -9,7 +9,6 @@ export default function Header() {
           <ul className="flex space-x-4">
             <Link href={"/"}>Home</Link>
             <Link href={"posts"}>Posts</Link>
-            <Link href={"photos"}>Photos</Link>
             <Link href={"about"}>About</Link>
           </ul>
         </nav>
